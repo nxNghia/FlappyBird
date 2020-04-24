f785c454fe03bc561e6bacab6de3f3ab8d699a41
NghiaNguyen <nghia1421999@gmail.com>
2020-04-24 10:14:17 +0700

    second commit

--- flappy.js
+++ flappy.js
@@ -6,6 +6,16 @@ var obstacle = [];
 var collumn = 1;
 var score = 0;
 
+function Ball(height, vel_up, gravity)
+{
+	this.height = height;
+	this.velocity_up = vel_up;
+	this.cur_height = height;
+	this.cur_velocity = vel_up;
+	this.gravity = gravity;
+}
+var aBall = new Ball(400, 20, 2);
+
 function box(height_bottom, x)
 {
 	this.x = x;
@@ -25,6 +35,32 @@ obstacle.push(box3);
 obstacle.push(box4);
 obstacle.push(box5);
 
+ctx.beginPath();
+ctx.clearRect(0, 0, 800, 800);
+ctx.arc(200, aBall.cur_height, 10, 0, 2 * Math.PI);
+ctx.fillStyle = "#ffffff";
+ctx.lineWidth = 2;
+ctx.strokeStyle = "#ffffff";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+for (var i = obstacle.length - 1; i >= 0; i--)
+{
+	ctx.fillStyle = "#000000";
+	ctx.fillRect(obstacle[i].x + 5, obstacle[i].height_bottom + 48, 90, 800 - obstacle[i].height_bottom - 48);
+	ctx.fillRect(obstacle[i].x, obstacle[i].height_bottom, 100, 48);
+	ctx.fillRect(obstacle[i].x + 5, 0, 90, obstacle[i].height_top - 48);
+	ctx.fillRect(obstacle[i].x, obstacle[i].height_top - 48, 100, 48);
+
+	ctx.fillStyle = "#009900";
+	ctx.fillRect(obstacle[i].x + 4, obstacle[i].height_bottom + 4, 100 - 8, 40);
+	ctx.fillRect(obstacle[i].x + 4 + 5, obstacle[i].height_bottom + 4 + 44, 100 - 8 - 10, 800 - obstacle[i].height_bottom - 4 - 40);
+	ctx.fillRect(obstacle[i].x + 4 + 5, 0, 82, obstacle[i].height_top - 8 - 40);
+	ctx.fillRect(obstacle[i].x + 4, obstacle[i].height_top - 40 - 4 , 92, 40);
+}
+ctx.stroke();
+
 function draw(object1, object2)
 {
 	ctx.beginPath();
@@ -40,23 +76,20 @@ function draw(object1, object2)
 	for (var i = object2.length - 1; i >= 0; i--)
 	{
 		ctx.fillStyle = "#000000";
-		ctx.fillRect(object2[i].x, object2[i].height_bottom, 100, 800 - object2[i].height_bottom);
-		ctx.fillRect(object2[i].x, 0, 100, object2[i].height_top);
+		ctx.fillRect(object2[i].x + 5, object2[i].height_bottom + 48, 90, 800 - object2[i].height_bottom - 48);
+		ctx.fillRect(object2[i].x, object2[i].height_bottom, 100, 48);
+		ctx.fillRect(object2[i].x + 5, 0, 90, object2[i].height_top - 48);
+		ctx.fillRect(object2[i].x, obstacle[i].height_top - 48, 100, 48);
+		
 		ctx.fillStyle = "#009900";
-		ctx.fillRect(object2[i].x + 4, object2[i].height_bottom + 4, 100 - 8, 800 - object2[i].height_bottom - 4);
-		ctx.fillRect(object2[i].x + 4, 0, 92, object2[i].height_top - 4);
+		ctx.fillRect(object2[i].x + 4, object2[i].height_bottom + 4, 100 - 8, 40);
+		ctx.fillRect(object2[i].x + 4 + 5, object2[i].height_bottom + 4 + 44, 100 - 8 - 10, 800 - object2[i].height_bottom - 4 - 40);
+		ctx.fillRect(object2[i].x + 4 + 5, 0, 82, object2[i].height_top - 8 - 40);
+		ctx.fillRect(object2[i].x + 4, object2[i].height_top - 40 - 4 , 92, 40);
 	}
 	ctx.stroke();
 }
 
-function Ball(height, vel_up, gravity)
-{
-	this.height = height;
-	this.velocity_up = vel_up;
-	this.cur_height = height;
-	this.cur_velocity = vel_up;
-	this.gravity = gravity;
-}
 var t = 1;
 document.onkeydown = function(envent)
 {
@@ -68,18 +101,20 @@ document.onkeydown = function(envent)
 	}
 }
 
-var aBall = new Ball(200, 20, 2);
-var already_jump = false;
+var finished = false;
 
 function reset()
 {
 	ctx.clearRect(0, 0, 800, 800);
-	already_jump = false;
+	finished = false;
 	aBall.height = 400;
 	aBall.cur_height = 400;
 	aBall.velocity_up = 20;
 	aBall.cur_velocity = 20;
+	aBall.gravity = 2;
 	score = 0;
+	collumn = 1;
+	t = 0;
 
 	for (var i = obstacle.length - 1; i >= 0; i--)
 	{
@@ -87,13 +122,17 @@ function reset()
 		obstacle[i].height_bottom = Math.floor(Math.random() * 200 + 400);
 		obstacle[i].height_top = obstacle[i].height_bottom - 200;
 	}
+
+	draw(aBall, obstacle);
 }
+
+
 
 function jump()
 {
-	if(already_jump == false)
+	if(finished == false)
 	{
-		already_jump = true;
+		finished = true;
 		var _frame = 0;
 		var id = setInterval(frame, 30);
 		function frame()
@@ -133,6 +172,9 @@ function jump()
 
 		function GameOver()
 		{
+			if(aBall.cur_height <= 10 || aBall.cur_height >= 780)
+				return true;
+
 			if(obstacle[collumn - 1].x >= 100 && obstacle[collumn - 1].x <= 200)
 			{
 				if(aBall.cur_height <= obstacle[collumn - 1].height_top + 10 || aBall.cur_height >= obstacle[collumn - 1].height_bottom - 10)
@@ -142,7 +184,7 @@ function jump()
 			}else{
 				if(aBall.cur_height >= obstacle[collumn - 1].height_bottom || aBall.cur_height <= obstacle[collumn - 1].height_top)
 				{
-					if(obstacle[collumn - 1].x - 10 <= 200)
+					if(obstacle[collumn - 1].x - 5 <= 200)
 					{
 						return true;
 					}
@@ -170,6 +212,14 @@ function jump()
 					return true;
 				
 				distance = (obstacle[collumn - 2].x + 100 - 200)**2 + (aBall.cur_height - obstacle[collumn - 2].height_top)**2;
+				if(distance <= 100)
+					return true;
+			}else{
+				var distance = (obstacle[4].x + 100 - 200)**2 + (aBall.cur_height - obstacle[4].height_bottom)**2;
+				if(distance <= 100)
+					return true;
+				
+				distance = (obstacle[4].x + 100 - 200)**2 + (aBall.cur_height - obstacle[4].height_top)**2;
 				if(distance <= 100)
 					return true;
 			}
